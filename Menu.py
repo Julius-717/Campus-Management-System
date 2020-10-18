d91d2932241719056d61cd180ccafa0cb91134fb
Julius-717 <waynejulius10@gmail.com>
2020-10-18 23:54:43 +0300

    add library function

--- Menu.py
+++ Menu.py
@@ -4,3 +4,12 @@ from tkinter import *
 import random
 import os
 
+def __marksheet__():
+   filename = 'Search_Page.py'
+   os.system(filename)
+   os.system('vscode'+filename)
+
+def __Library__():
+    filename = 'Library_Frontend.py'
+    os.system(filename)
+    os.system('vscode'+filename)
