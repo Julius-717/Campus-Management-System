53a56d31b95c3bf973f2dcc4831a3b78a8b3e35e
Julius-717 <waynejulius10@gmail.com>
2020-10-18 22:51:59 +0300

    added connect function

--- Std_info_Backend.py
+++ Std_info_Backend.py
@@ -1,3 +1,13 @@
 # Campus-Management-System
 # import database
 import sqlite3
+
+def connect():
+    conn = sqlite3.connect("student.db")
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY, name text, fname text, mname text, \
+        address text, mobno integer,email text, dob integer, gender text)")
+
+    conn.commit()
+    conn.close()
