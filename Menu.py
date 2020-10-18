0b4b1b3eb7c93d631dcb13dafdfa13632c1251eb
Julius-717 <waynejulius10@gmail.com>
2020-10-19 00:01:19 +0300

    added frames

--- Menu.py
+++ Menu.py
@@ -35,3 +35,13 @@ def menu():
        
     title_Label = Label(title_Frame, text = 'MENU', font = ('arial',30,'bold'), bg = 'navajo white')
     title_Label.grid(row = 0, column = 0, padx = 150)
+
+    # Frame5
+    Frame_1 = LabelFrame(root, font = ('arial',17,'bold'), width = 1000, height = 100, bg = 'navajo white', relief = 'ridge', bd = 10)
+    Frame_1.grid(row = 1, column = 0, padx = 280)
+    Frame_2 = LabelFrame(root, font = ('arial',17,'bold'), width = 1000, height = 100, bg = 'navajo white', relief = 'ridge', bd = 10)
+    Frame_2.grid(row = 2, column = 0, padx = 130, pady = 7)
+    Frame_3 = LabelFrame(root, font = ('arial',17,'bold'), width = 1000, height = 100, bg = 'navajo white', relief = 'ridge', bd = 10)
+    Frame_3.grid(row = 3, column = 0, pady = 7)
+    Frame_4 = LabelFrame(root, font = ('arial',17,'bold'), width = 1000, height = 100, bg = 'navajo white', relief = 'ridge', bd = 10)
+    Frame_4.grid(row = 4, column = 0, pady = 7)
