538cd2f6993d5fc089bca3e29d91f57556945e1b
Julius-717 <waynejulius10@gmail.com>
2020-10-19 13:52:50 +0300

    added compute function for frame1

--- Marksheet_Frontend.py
+++ Marksheet_Frontend.py
@@ -304,3 +304,59 @@ def search_result_marksheet(row):
     root.title('Marksheet')
     root.geometry('1350x750')
     root.config(bg='Navajo white')
+
+    # Functions2
+    def Compute():
+        x1 = (m1.get());      x2 = (m2.get());    x3 = (m3.get());      x4 = (m4.get());    x5 = (m5.get())                                   
+       
+        tot = x1+x2+x3+x4+x5
+        gt.set(tot)
+              
+        Per = ((x1+x2+x3+x4+x5) * 100)/500
+        per.set(Per)
+
+        cg = (((x1+x2+x3+x4+x5) * 100)/500) / 9.5
+        cgpa.set(round(cg,1))
+
+        if (((x1+x2+x3+x4+x5) * 100)/500) <= 40:
+            grd = 'G'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 50:
+            grd = 'F'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 60:
+            grd = 'E'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 70:
+            grd = 'D'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 80:
+            grd = 'C'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 90:
+            grd = 'B'
+        else:
+            grd = 'A'
+
+        grade.set(grd)
+
+        count = 0
+        if x1 < 33:
+            count = count + 1
+        if x2 < 33:
+            count = count + 1
+        if x3 < 33:
+            count = count + 1
+        if x4 < 33:
+            count = count + 1
+        if x5 < 33:
+            count = count + 1
+
+        if (count == 0):
+            result.set('PASS')
+        elif (count == 1 or count == 2 ):
+            result.set('SUPPLY')
+        else:
+            result.set('FAIL')
+
+        if Per <= 45 and result != "FAIL":
+            div.set('THIRD')
+        elif Per <= 60 and result != "FAIL":
+            div.set('SECOND')
+        elif Per <= 100:
+            div.set('FIRST')     
