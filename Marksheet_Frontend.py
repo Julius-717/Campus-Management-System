283d58cc493167ea7efc30eef7428d78191c9b52
Julius-717 <waynejulius10@gmail.com>
2020-10-19 13:29:08 +0300

    added compute function

--- Marksheet_Frontend.py
+++ Marksheet_Frontend.py
@@ -51,3 +51,77 @@ def marksheet():
         if Exit > 0:
             root.destroy()
             return
+
+    def Compute():
+        x1 = (m1.get());      x2 = (m2.get());    x3 = (m3.get());      x4 = (m4.get());    x5 = (m5.get())
+
+        if x1 > 100:
+            tkinter.messagebox.askokcancel('Attention', 'Please enter Correct Marks')
+            return
+        if x2 > 100:
+            tkinter.messagebox.askokcancel('Attention', 'Please enter Correct Marks')
+            return
+        if x3 > 100:
+            tkinter.messagebox.askokcancel('Attention', 'Please enter Correct Marks')
+            return
+        if x4 > 100:
+            tkinter.messagebox.askokcancel('Attention', 'Please enter Correct Marks')
+            return
+        if x5 > 100:
+            tkinter.messagebox.askokcancel('Attention', 'Please enter Correct Marks')
+            return
+
+        tot = x1+x2+x3+x4+x5
+        gt.set(tot)
+
+        Per = ((x1+x2+x3+x4+x5) * 100)/500
+        per.set(Per)
+
+        cg = (((x1+x2+x3+x4+x5) * 100)/500) / 9.5
+        cgpa.set(round(cg, 1))
+
+        if cg > 10:
+            cgpa.set(10)
+
+        if (((x1+x2+x3+x4+x5) * 100)/500) <= 40:
+            grd = 'G'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 50:
+            grd = 'F'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 60:
+            grd = 'E'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 70:
+            grd = 'D'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 80:
+            grd = 'C'
+        elif (((x1+x2+x3+x4+x5) * 100)/500) <= 90:
+            grd = 'B'
+        else:
+            grd = 'A'
+
+        grade.set(grd)
+
+        count = 0
+        if x1 < 33:
+            count = count + 1
+        if x2 < 33:
+            count = count + 1
+        if x3 < 33:
+            count = count + 1
+        if x4 < 33:
+            count = count + 1
+        if x5 < 33:
+            count = count + 1
+        
+        if (count == 0):
+            result.set('PASS')
+        elif (count == 1 or count == 2):
+            result.set('SUPPLY')
+        else:
+            result.set('FAIL')
+        
+        if Per <= 45 and result != "FAIL":
+            div.set('THIRD')
+        elif Per <= 60 and result != "FAIL":
+            div.set('SECOND')
+        elif Per <= 100:
+            div.set('FIRST')
