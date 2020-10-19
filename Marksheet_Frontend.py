d56e50ff46b37a3de95b950249e08672dbd3eedc
Julius-717 <waynejulius10@gmail.com>
2020-10-19 12:01:00 +0300

    added marksheet function

--- Marksheet_Frontend.py
+++ Marksheet_Frontend.py
@@ -5,3 +5,9 @@ import Marksheet_Backend
 import random
 import tkinter.messagebox
 from tkinter import ttk
+
+def marksheet():
+    root = Tk()
+    root.title('Marksheet')
+    root.geometry('1350x750')
+    root.config(bg = 'Navajo white')
