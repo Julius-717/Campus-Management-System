b0d5d8abc5a9ffffea5206f3c38bf093e1b38b33
Julius-717 <waynejulius10@gmail.com>
2020-10-19 14:09:39 +0300

    added connect function

--- Marksheet_Backend.py
+++ Marksheet_Backend.py
@@ -3,3 +3,13 @@
 from tkinter import *
 import sqlite3
 
+def connect():
+    con = sqlite3.connect('Marks.db')
+    cur = con.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS Marks (id INTEGER PRIMARY KEY, name text, roll integer, fname text, mname \
+        text, DOB integer, gender text, scl text, email text, m1 integer, m2 integer, m3 integer, m4 integer, \
+            m5 integer, gt integer, per integer, cgpa integer, grade text, div text, result text)')
+
+    con.commit()
+    con.close()
